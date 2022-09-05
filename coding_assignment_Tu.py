9ba4c45ce860b64d181daed1d71bf34686d6ed47
aintreadyourpost <ductu97@gmail.com>
2022-09-05 16:25:13 +0700

    Fix

--- coding_assignment_Tu.py
+++ coding_assignment_Tu.py
@@ -30,22 +30,24 @@ if rooml != L:
 
 #Func changing the status of the room
 def roomstatchange():
+    global roomchoice, statchoice
     avasort = sorted(str(x) for x in ava)   
     occsort = sorted(str(x) for x in occ)
     ressort = sorted(str(x) for x in res)
-    print('\n----------------------------------------------------------------------')
-    print('\n                         <LIST OF ROOMS>')
-    print(f'\n{Fore.GREEN}{Style.BRIGHT}Available room(s):{Style.RESET_ALL}',seperator.join(avasort))
-    print(f'\n{Fore.RED}{Style.BRIGHT}Occupied room(s):{Style.RESET_ALL} ',seperator.join(occsort))
-    print(f'\n{Fore.YELLOW}{Style.BRIGHT}Reserved room(s):{Style.RESET_ALL} ',seperator.join(ressort))
-    print('\n----------------------------------------------------------------------')
+    prstat = f'''\n----------------------------------------------------------------------
+        \n                         <LIST OF ROOMS>
+        \n{Fore.GREEN}{Style.BRIGHT}Available room(s):{Style.RESET_ALL} {seperator.join(avasort)}
+        \n{Fore.RED}{Style.BRIGHT}Occupied room(s):{Style.RESET_ALL}  {seperator.join(occsort)}
+        \n{Fore.YELLOW}{Style.BRIGHT}Reserved room(s):{Style.RESET_ALL}  {seperator.join(ressort)}
+        \n----------------------------------------------------------------------'''
+    print(prstat)
     roomchoice = str.lower(input('\nPlease enter the room that you want to change: '))
     while roomchoice not in rooml:    #Input validation
         print('\n----- INVALID INPUT -----')
         print('Please re-enter your choice.')
         roomchoice = str.lower(input('\nPlease enter the room that you want to change: '))
     if roomchoice in ava:   #Changing room status if room is already availble
-        print('\nRoom',roomchoice,f'is {Fore.GREEN}{Style.BRIGHT}AVAILABLE{Style.RESET_ALL}.')
+        print(f'\nRoom {roomchoice} is {Fore.GREEN}{Style.BRIGHT}AVAILABLE{Style.RESET_ALL}.')
         statchoice = str.lower(input(f'\nDo you want to make it {Fore.RED}{Style.BRIGHT}OCCUPIED (O){Style.RESET_ALL} or {Fore.YELLOW}{Style.BRIGHT}RESERVED (R){Style.RESET_ALL}?: '))
         while statchoice not in ['o','r']:  #Input validity check
             print('\n----- INVALID INPUT -----')
@@ -63,8 +65,7 @@ def roomstatchange():
             file_ava.close()                    #
             file_occ.close()                    #
             print('\nRoom status changed successfully!')
-            print('Room',roomchoice,f'is now {Fore.RED}{Style.BRIGHT}OCCUPIED{Style.RESET_ALL}.')
-            returnorend()
+            print(f'Room {roomchoice} is now {Fore.RED}{Style.BRIGHT}OCCUPIED{Style.RESET_ALL}.')
         if statchoice == 'r':
             ava.remove(roomchoice)              
             res.append(roomchoice)
@@ -79,9 +80,9 @@ def roomstatchange():
             file_ava.close()
             file_res.close()
             print('\nRoom status changed successfully!')
-            print('\nRoom',roomchoice,f'is now {Fore.YELLOW}{Style.BRIGHT}RESERVED{Style.RESET_ALL}.')
-            returnorend()
-    if roomchoice in occ:   #Changing room status if room is already occupied
+            print(f'\nRoom {roomchoice} is now {Fore.YELLOW}{Style.BRIGHT}RESERVED{Style.RESET_ALL}.')
+        returnorend()
+    elif roomchoice in occ:   #Changing room status if room is already occupied
         print('\nRoom',roomchoice,f'is {Fore.RED}{Style.BRIGHT}OCCUPIED{Style.RESET_ALL}.')
         statchoice = str.lower(input(f'Do you want to make it {Fore.GREEN}{Style.BRIGHT}AVAILABLE{Style.RESET_ALL}? Enter Y for {Fore.GREEN}{Style.BRIGHT}yes{Style.RESET_ALL} or N for {Fore.RED}{Style.BRIGHT}no{Style.RESET_ALL}: '))
         while statchoice not in ['y','n']:  #Input validation
@@ -100,13 +101,12 @@ def roomstatchange():
             file_ava.close()
             file_occ.close()
             print('\nRoom status changed successfully!')
-            print('\nRoom',roomchoice,f'is now {Fore.GREEN}{Style.BRIGHT}AVAILABLE{Style.RESET_ALL}.')
-            returnorend()
+            print(f'\nRoom {roomchoice} is now {Fore.GREEN}{Style.BRIGHT}AVAILABLE{Style.RESET_ALL}.')
         if statchoice == 'n':
             print('\nRoom status remained.')
-            returnorend()  
-    if roomchoice in res:   #Changing room status if room is already reserved
-        print('\nRoom',roomchoice,f'is {Fore.YELLOW}{Style.BRIGHT}RESERVED{Style.RESET_ALL}.')
+        returnorend()  
+    elif roomchoice in res:   #Changing room status if room is already reserved
+        print(f'\nRoom {roomchoice} is {Fore.YELLOW}{Style.BRIGHT}RESERVED{Style.RESET_ALL}.')
         statchoice = str.lower(input(f'Do you want to make it {Fore.RED}{Style.BRIGHT}OCCUPIED (O){Style.RESET_ALL} or {Fore.GREEN}{Style.BRIGHT}AVAILABLE (A){Style.RESET_ALL}?: '))
         while statchoice not in ['a','o']:  #Input validation
             print('\n----- INVALID INPUT -----')
@@ -124,8 +124,7 @@ def roomstatchange():
             file_occ.close()
             file_res.close()
             print('\nRoom status changed successfully!')
-            print('\nRoom',roomchoice,f'is now {Fore.RED}{Style.BRIGHT}OCCUPIED{Style.RESET_ALL}.')
-            returnorend()
+            print(f'\nRoom {roomchoice} is now {Fore.RED}{Style.BRIGHT}OCCUPIED{Style.RESET_ALL}.')
         if statchoice == 'a':
             res.remove(roomchoice)
             ava.append(roomchoice)
@@ -138,58 +137,59 @@ def roomstatchange():
             file_ava.close()
             file_res.close()
             print('\nRoom status changed successfully!')
-            print('\nRoom',roomchoice,f'is now {Fore.GREEN}{Style.BRIGHT}AVAILABLE{Style.RESET_ALL}.')
-            returnorend()
-    
+            print(f'\nRoom {roomchoice} is now {Fore.GREEN}{Style.BRIGHT}AVAILABLE{Style.RESET_ALL}.')
+        returnorend()
+        
 #Func showing list of rooms and their statuses
 def roomlist():
     avasort = sorted(str(x) for x in ava)
     occsort = sorted(str(x) for x in occ)
     ressort = sorted(str(x) for x in res)
-    print('\n----------------------------------------------------------------------')
-    print('\n                         <LIST OF ROOMS>')
-    print(f'\n{Fore.GREEN}{Style.BRIGHT}Available room(s):{Style.RESET_ALL}',seperator.join(avasort))
-    print(f'\n{Fore.RED}{Style.BRIGHT}Occupied room(s):{Style.RESET_ALL} ',seperator.join(occsort))
-    print(f'\n{Fore.YELLOW}{Style.BRIGHT}Reserved room(s):{Style.RESET_ALL} ',seperator.join(ressort))
-    print('\n----------------------------------------------------------------------')
+    prstat = f'''\n----------------------------------------------------------------------
+        \n                         <LIST OF ROOMS>
+        \n{Fore.GREEN}{Style.BRIGHT}Available room(s):{Style.RESET_ALL} {seperator.join(avasort)}
+        \n{Fore.RED}{Style.BRIGHT}Occupied room(s):{Style.RESET_ALL}  {seperator.join(occsort)}
+        \n{Fore.YELLOW}{Style.BRIGHT}Reserved room(s):{Style.RESET_ALL}  {seperator.join(ressort)}
+        \n----------------------------------------------------------------------'''
+    print(prstat)
     returnorend()
     
 #Return to main menu or terminate program
 def returnorend():
     print('\nDo you want to return to main menu or end your session?')
     menu = input('Enter 1 to return to main menu, or 2 to end your session: ')
-    while menu not in ['1','2']:    #Input validation
+    while menu not in ('1','2'):    #Input validation
         print('\n----- INVALID INPUT -----')
         menu = input('\nEnter 1 to return to main menu, or 2 to end your session: ')
     if menu == '1':
         mainmenu()
-    if menu == '2':
+    elif menu == '2':
         print(f'\n{Fore.BLACK}{Back.WHITE}We hope to see you again! Good bye!{Style.RESET_ALL}')
         sys.exit()
       
 #Menu mapping
 def mainmenu():
     global menuchoice
-    print('\n----------------------------------------------------------------------')
-    print(f'\n                    {Style.BRIGHT}{Fore.RED}{Back.WHITE}WELCOME TO BUV SUNSHINE HOTEL{Style.RESET_ALL}')
-    print('\n                              MAIN MENU')
-    print('\n1. View list of rooms.')
-    print('\n2. Change room status.')
-    print('\n3. Show hotel map.')
-    print('\n4. Exit')
-    print('\n----------------------------------------------------------------------')
+    prmain = f'''\n----------------------------------------------------------------------
+        \n                    {Style.BRIGHT}{Fore.RED}{Back.WHITE}WELCOME TO BUV SUNSHINE HOTEL{Style.RESET_ALL}
+        \n                              MAIN MENU
+        \n1. View list of rooms.
+        \n2. Change room status.
+        \n3. Show hotel map.
+        \n4. Exit
+        \n----------------------------------------------------------------------'''
+    print(prmain)
     menuchoice = input('\nChoose your option: ')
     while menuchoice not in ('1','2','3','4'): #Input validation
         menuchoice = input('\nPlease enter a valid choice: ')
     if menuchoice == '1':
         roomlist()
-    if menuchoice == '2':
+    elif menuchoice == '2':
         roomstatchange()
-    if menuchoice == '3': 
-        pass
-    if menuchoice == '4':
-        print('\n----------------------------------------------------------------------')
-        print(f'\n{Fore.BLACK}{Back.WHITE}We hope to see you again! Good bye!{Style.RESET_ALL}')
+    elif menuchoice == '3':
+        pass   
+    elif menuchoice == '4':
+        print(f'\n----------------------------------------------------------------------\n{Fore.BLACK}{Back.WHITE}We hope to see you again! Good bye!{Style.RESET_ALL}')
         sys.exit()
         
 #Program running              
